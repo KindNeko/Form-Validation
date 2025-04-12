1350406f1f1ec0bb5cf244d6ca337676a0fe5de7
Maxim Pantelidi <maksim.pantelidi@yandex.ru>
2025-04-12 20:25:10 +0300

    Добавил JSDoc, не много порефачил код вынеся логику из слушителя событий в отдельный метод

--- js/app.js
+++ js/app.js
@@ -4,17 +4,27 @@ const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirm-password');
 
+/**
+ * Удаляет указанные классы у родительского элемента и добавляет новый класс.
+ * @param {HTMLElement} element - Элемент, у которого будет изменён родительский элемент.
+ * @param {string[]} removeClasses - Массив классов для удаления (по умолчанию пустой).
+ * @param {string} addClass - Класс для добавления (по умолчанию пустая строка).
+ */
 const removeParentClassesAndAddAnother = (
 	element,
 	removeClasses = [],
 	addClass = ''
 ) => {
 	const parent = element.parentElement;
-
 	parent.classList.remove(...removeClasses);
 	parent.classList.add(addClass);
 };
 
+/**
+ * Показывает сообщение об ошибке для поля ввода.
+ * @param {HTMLInputElement} input - Поле ввода, для которого показывается ошибка.
+ * @param {string} message - Текст сообщения об ошибке.
+ */
 const showError = (input, message) => {
 	const formControl = input.parentElement;
 	removeParentClassesAndAddAnother(input, ['success', 'error'], 'error');
@@ -24,16 +34,29 @@ const showError = (input, message) => {
 	}
 };
 
+/**
+ * Показывает успешное состояние для поля ввода.
+ * @param {HTMLInputElement} input - Поле ввода, для которого показывается успешное состояние.
+ */
 const showSuccess = (input) => {
 	removeParentClassesAndAddAnother(input, ['success', 'error'], 'success');
 };
 
+/**
+ * Проверяет, является ли email допустимым.
+ * @param {string} email - Строка для проверки.
+ * @returns {boolean} - True, если email валиден, иначе false.
+ */
 const isValidEmail = (email) => {
 	const regex =
 		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return regex.test(String(email).toLowerCase());
 };
 
+/**
+ * Проверяет, заполнены ли все обязательные поля.
+ * @param {HTMLInputElement[]} inputArr - Массив полей ввода для проверки.
+ */
 const checkRequired = (inputArr) => {
 	inputArr.forEach((input) => {
 		if (input.value.trim() === '') {
@@ -43,9 +66,14 @@ const checkRequired = (inputArr) => {
 		}
 	});
 };
-
-form.addEventListener('submit', (event) => {
+/**
+ * Обработчик события  submit формы.
+ * @param {Event} event - Событие submit.
+ */
+const handleFormSubmit = (event) => {
 	event.preventDefault();
 
 	checkRequired([userName, email, password, confirmPassword]);
-});
+};
+
+form.addEventListener('submit', handleFormSubmit);
